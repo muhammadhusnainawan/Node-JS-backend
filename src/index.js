5e2e5de5ebc1546f75c93dd67d3836ec09a75822
muhammadhusnainawan <awan.husnain@gmail.com>
2023-12-22 23:11:39 +0500

    setup of utils

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import dotenv from "dotenv";
 import connectDb from "./db/index.js"
+import {app} from "./app.js"
 
 // dotenv config
 dotenv.config({path:"./.env"})
 
 // conect db
 connectDb()
+.then(()=>{
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(`Server is running on PORT : ${process.env.PORT}`)
+    })
+})
+.catch((error)=>{
+    console.log("MonogDb connection failed !!! ", error)
+})
