14c17ef915cf47a3511dd5f3589ce3e1aec78adc
muhammadhusnainawan <awan.husnain@gmail.com>
2024-01-13 00:54:14 +0500

    import video endpoint

--- src/app.js
+++ src/app.js
@@ -16,12 +16,14 @@ app.use(cookieParser())
 // routes imports
 import userRouter from "./routes/user.routes.js"
 import subscriptionRouter from "./routes/subscription.routes.js"
+import videoRouter from "./routes/video.routes.js"
 
 
 
 // routes declaration
 app.use("/api/v1/users", userRouter)
 app.use("/api/v1/subscribers", subscriptionRouter)
+app.use("/api/v1/videos", videoRouter)
 
 
 
