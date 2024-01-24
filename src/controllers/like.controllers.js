f10219d27cdda52189e53535a72782a3aafd452e
muhammadhusnainawan <awan.husnain@gmail.com>
2024-01-24 22:53:31 +0500

    defined toggleCommentLike endpoint

--- src/controllers/like.controllers.js
+++ src/controllers/like.controllers.js
@@ -28,6 +28,9 @@ const toggleVideoLike = asyncHandler(async (req, res) => {
       video: new mongoose.Types.ObjectId(videoId),
       likedBy: new mongoose.Types.ObjectId(req.user?._id),
     });
+    if (!videoLike) {
+      throw new ApiError(400, "video is not liked try again");
+    }
     like = true;
   }
   res
@@ -43,4 +46,44 @@ const toggleVideoLike = asyncHandler(async (req, res) => {
     );
 });
 
-export { toggleVideoLike };
+const toggleCommentLike = asyncHandler(async (req, res) => {
+  const { commentId } = req.params;
+  // check if comment id is a valid object id otherwise throw error
+  // findOne document on the basis of comment id and user
+  // if already liked the comment then find it and delete
+  // if not liked the comment the create the document
+  if (!isValidObjectId(commentId)) {
+    throw new ApiError(400, "Invalid comment id");
+  }
+  const isCommentLiked = await Like.findOne({
+    $and: [
+      { comment: new mongoose.Types.ObjectId(commentId) },
+      { likedBy: new mongoose.Types.ObjectId(req.user?._id) },
+    ],
+  });
+  let like;
+  if (isCommentLiked) {
+    await Like.findByIdAndDelete(isCommentLiked?._id);
+    like = false;
+  } else {
+    const commentLike = await Like.create({
+      comment: new mongoose.Types.ObjectId(commentId),
+      likedBy: new mongoose.Types.ObjectId(req.user?._id),
+    });
+    if (!commentId) {
+      throw new ApiError(400, "comment is not liked please try again");
+    }
+    like = true;
+  }
+  res
+    .status(200)
+    .json(
+      new ApiResponse(
+        200,
+        {},
+        like ? "comment is liked by user" : "comment is unliked by user"
+      )
+    );
+});
+
+export { toggleVideoLike,toggleCommentLike };
--- src/routes/like.routes.js
+++ src/routes/like.routes.js
@@ -1,10 +1,14 @@
 import { Router } from "express";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
-import { toggleVideoLike } from "../controllers/like.controllers.js";
+import {
+  toggleVideoLike,
+  toggleCommentLike,
+} from "../controllers/like.controllers.js";
 const router = Router();
 
 router.use(verifyJWT); // Apply verifyJwt to all routes in this file
 
 router.route("/toggle/video/:videoId").post(toggleVideoLike);
+router.route("/toggle/comment/:commentId").post(toggleCommentLike);
 
 export default router;
