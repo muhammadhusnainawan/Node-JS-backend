3ed18925953680ffa9c1b3135659d7545959cce2
muhammadhusnainawan <awan.husnain@gmail.com>
2024-01-24 22:36:42 +0500

    Configure and Integrate like Router

--- src/app.js
+++ src/app.js
@@ -1,30 +1,29 @@
-import express from "express"
-import cors from "cors"
+import express from "express";
+import cors from "cors";
 import cookieParser from "cookie-parser";
 
-
 const app = express();
-app.use(cors({
+app.use(
+  cors({
     origin: process.env.CORS_ORIGIN,
-    credentials: true
-}))
-app.use(express.json({limit:"16kb"}))
-app.use(express.urlencoded({extended: true,limit:"16kb"}))
-app.use(express.static("public"))
-app.use(cookieParser())
+    credentials: true,
+  })
+);
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+app.use(cookieParser());
 
 // routes imports
-import userRouter from "./routes/user.routes.js"
-import subscriptionRouter from "./routes/subscription.routes.js"
-import videoRouter from "./routes/video.routes.js"
-
-
+import userRouter from "./routes/user.routes.js";
+import subscriptionRouter from "./routes/subscription.routes.js";
+import videoRouter from "./routes/video.routes.js";
+import likeRouter from "./routes/like.routes.js";
 
 // routes declaration
-app.use("/api/v1/users", userRouter)
-app.use("/api/v1/subscribers", subscriptionRouter)
-app.use("/api/v1/videos", videoRouter)
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/subscribers", subscriptionRouter);
+app.use("/api/v1/videos", videoRouter);
+app.use("/api/v1/likes", likeRouter);
 
-
-
-export {app}
+export { app };
