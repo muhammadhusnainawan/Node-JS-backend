4f5245b9944c5056c4851433f5a4b788e969c02a
muhammadhusnainawan <awan.husnain@gmail.com>
2024-01-27 22:12:00 +0500

    defined getVideoComments endpoint

---
+++ src/controllers/comment.controllers.js
@@ -0,0 +1,37 @@
+import { Comment } from "../models/comment.models.js";
+import mongoose, { isValidObjectId } from "mongoose";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { ApiError } from "../utils/ApiError.js";
+
+const getVideoComments = asyncHandler(async (req, res) => {
+  // get all comment of a video
+  const videoId = req.params;
+  const { page = 1, limit = 10 } = req.query;
+
+  const commentaggregate = Comment.aggregate([
+    {
+      $match: {
+        video: new mongoose.Types.ObjectId(videoId),
+      },
+    },
+    {
+      $lookup: {
+        from: "users",
+        localField: "owner",
+        foreignField: "_id",
+        as: "owner",
+      },
+    },
+    {
+      $lookup: {
+        from: "likes",
+        localField: "_id",
+        foreignField: "comment",
+        as: "likes",
+      },
+    },
+  ]);
+});
+
+export { getVideoComments };
