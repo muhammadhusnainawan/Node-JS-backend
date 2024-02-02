7e5dd49d89f122d33407ae1af51c9cd72ae48244
muhammadhusnainawan <awan.husnain@gmail.com>
2024-02-02 22:22:45 +0500

    configure nd integrate dashboard router

--- src/app.js
+++ src/app.js
@@ -22,6 +22,7 @@ import likeRouter from "./routes/like.routes.js";
 import commentRouter from "./routes/comment.routes.js";
 import tweetRouter from "./routes/tweet.routes.js";
 import playlistRouter from "./routes/playlist.routes.js";
+import dashboardRouter from "./routes/dashboard.routes.js";
 
 // routes declaration
 app.use("/api/v1/users", userRouter);
@@ -31,5 +32,6 @@ app.use("/api/v1/likes", likeRouter);
 app.use("/api/v1/comments", commentRouter);
 app.use("/api/v1/tweets", tweetRouter);
 app.use("/api/v1/playlists", playlistRouter);
+app.use("/api/v1/dashboard", dashboardRouter);
 
 export { app };
