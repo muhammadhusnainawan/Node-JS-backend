b7244e07d018f8ccd770dd042906e6b49a0d3b83
muhammadhusnainawan <awan.husnain@gmail.com>
2024-02-03 23:14:46 +0500

    defined healthCheck endpoint

---
+++ src/controllers/healthcheck.controllers.js
@@ -0,0 +1,12 @@
+import {asyncHandler} from "../utils/asyncHandler.js"
+import {ApiResponse} from "../utils/ApiResponse.js"
+
+const healthCheck = asyncHandler( async(req,res)=>{
+
+    res
+        .status(200)
+        .json( new ApiResponse(200,{message:"Everything is O.K"},"Ok"  ) )
+} )
+
+
+export {healthCheck}
