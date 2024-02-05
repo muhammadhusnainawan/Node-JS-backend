6a5bec443c0a6541824e32074abb0e89dd7ffe87
muhammadhusnainawan <awan.husnain@gmail.com>
2024-02-05 22:58:21 +0500

    defined getAllVideos endpoint, completed all endpoints

--- src/controllers/dashboard.controllers.js
+++ src/controllers/dashboard.controllers.js
@@ -1,7 +1,7 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 import mongoose, { isValidObjectId } from "mongoose";
 import { Video } from "../models/video.models.js";
-import {Subscription} from "../models/subscription.models.js"
+import { Subscription } from "../models/subscription.models.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 
@@ -48,36 +48,34 @@ const getChannelStats = asyncHandler(async (req, res) => {
           $sum: "$views",
         },
         totalVideos: {
-            $sum: 1
+          $sum: 1,
+        },
       },
-    }
-}
+    },
   ]);
 
-  const subscribers = await Subscription.aggregate(
-    [
-        {
-            $match:{
-                channel: new mongoose.Types.ObjectId(userId)
-            }
+  const subscribers = await Subscription.aggregate([
+    {
+      $match: {
+        channel: new mongoose.Types.ObjectId(userId),
+      },
+    },
+    {
+      $group: {
+        _id: null,
+        subscribersCount: {
+          $sum: 1,
         },
-        {
-            $group:{
-                _id:null,
-                subscribersCount:{
-                    $sum:1
-                }
-            }
-        }
-    ]
-  )
+      },
+    },
+  ]);
 
   const channelStats = {
     subscribers: subscribers[0]?.subscribersCount || 0,
     totalLikes: video[0]?.totalLikes || 0,
     totalViews: video[0]?.totalViews || 0,
-    totalVideos: video[0]?.totalVideos || 0
-};
+    totalVideos: video[0]?.totalVideos || 0,
+  };
   res
     .status(200)
     .json(
@@ -85,4 +83,48 @@ const getChannelStats = asyncHandler(async (req, res) => {
     );
 });
 
-export { getChannelStats };
+const getAllVideos = asyncHandler(async (req, res) => {
+  const videos = await Video.aggregate([
+    {
+      $match: {
+        owner: new mongoose.Types.ObjectId(req.user?._id),
+      },
+    },
+    {
+      $lookup: {
+        from: "likes",
+        localField: "_id",
+        foreignField: "video",
+        as: "likes",
+      },
+    },
+    {
+      $addFields: {
+        likesCount: {
+          $size: "$likes",
+        },
+      },
+    },
+
+    {
+      $project: {
+        _id: 1,
+        "videoFile.url": 1,
+        "thumbnail.url": 1,
+        title: 1,
+        description: 1,
+        createdAt: 1,
+        isPublished: 1,
+        likesCount: 1,
+      },
+    },
+  ]);
+  if (!videos) {
+    throw new ApiError(500,"Channel videos not fetched please try again")
+  }
+  res
+    .status(200)
+    .json( new ApiResponse(200,videos,"Channel videos fetched successfully") )
+});
+
+export { getChannelStats,getAllVideos };
--- src/routes/dashboard.routes.js
+++ src/routes/dashboard.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { getChannelStats } from "../controllers/dashboard.controllers.js";
+import { getChannelStats,getAllVideos } from "../controllers/dashboard.controllers.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
@@ -7,5 +7,6 @@ const router = Router();
 router.use(verifyJWT); //applying verifyJWT to all routes
 
 router.route("/channelstats").get(getChannelStats);
+router.route("/channelvideos").get(getAllVideos);
 
 export default router;
